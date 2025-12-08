6994708e1031baa3b13f9899086dc65089e6d867
DVISNAKO <dmitrijs.visnakovs@inbox.lv>
2025-12-08 20:28:06 +0200

    cikli

---
+++ 4.cicle.py
@@ -0,0 +1,27 @@
+for x in range(5):
+    print(x)
+
+for i in ['яблоко', 'банан', 'вишня']:
+    print(i)
+
+#while
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1
+print("Цикл завершён")
+
+for i in range(5):
+    if i == 3:
+        break
+print(i)
+
+for i in range(3):
+    pass
+print(i)
+
+x = 0
+while x < 3:
+    x += 1
+print(x)
